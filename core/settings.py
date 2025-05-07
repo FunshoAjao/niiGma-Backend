48fe2065cba950a6e46241ba2049969613681e92
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2025-05-08 00:57:06 +0100

    fix

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,6 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
