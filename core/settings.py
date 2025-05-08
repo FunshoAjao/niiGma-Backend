92d9c75773588853c3999d2368b32e149e70ed56
Osinaike Gbemileke <osinaikegbemileke@gmail.com>
2025-05-08 08:42:40 +0100

    fix: Update Sentry DSN and remove unused tracing configuration

--- core/settings.py
+++ core/settings.py
@@ -207,17 +207,8 @@ CACHES = {
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://c3b0121ee89f335b47ae12d4f39de32e@o4508864956858368.ingest.us.sentry.io/4508864960200704",
+    dsn="https://89326407107b0a6506a6a83b2511ab05@o4508864956858368.ingest.us.sentry.io/4509286400786432",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    _experiments={
-        # Set continuous_profiling_auto_start to True
-        # to automatically start the profiler on when
-        # possible.
-        "continuous_profiling_auto_start": True,
-    },
 )
