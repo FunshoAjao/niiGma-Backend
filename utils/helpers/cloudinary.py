f9962f919fa1c11066918400cef024b039969741
Osigbemes <osinaikegbemileke@gmail.com>
2025-06-12 14:23:54 +0100

    Refactor email verification template and integrate Cloudinary for file uploads
    - Updated the email verification HTML template to improve structure and styling, changing the branding from lab7ai to Niigma.
    - Added Cloudinary configuration settings (API_KEY, API_SECRET, CLOUD_NAME) to core settings.
    - Included Cloudinary as a dependency in requirements.txt.
    - Created a new helper module for Cloudinary file uploads, implementing methods for uploading files and handling exceptions.

---
+++ utils/helpers/cloudinary.py
@@ -0,0 +1,44 @@
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from cloudinary.uploader import upload
+from django.conf import settings
+
+
+
+class CloudinaryFileUpload:
+    def __init__(self):
+        self.cloud_name = settings.CLOUD_NAME
+        self.api_key = settings.API_KEY
+        self.api_secret = settings.API_SECRET
+        
+    def __set_up(self):
+        cloudinary.config( 
+            cloud_name = self.cloud_name, 
+            api_key = self.api_key, 
+            api_secret = self.api_secret,
+            secure=True
+        )
+        
+    def upload_file_to_cloudinary_v1(self, file)->str:
+        self.__set_up()
+        result = upload(file, resource_type="auto", type="upload")
+        return result["secure_url"]
+    
+    def upload_file_to_cloudinary(self, file, filename="upload.zip"):
+        """
+        Uploads a file or BytesIO stream to Cloudinary and returns the secure URL.
+        """
+        self.__set_up()
+        try:
+            result = cloudinary.uploader.upload(
+                file,
+                resource_type="raw",  # use 'raw' for non-image/video files like zip
+                public_id=filename,
+                overwrite=True,
+                use_filename=True,
+                unique_filename=False
+            )
+            return result["secure_url"]
+        except Exception as e:
+            raise Exception(f"Cloudinary upload failed: {e}")
